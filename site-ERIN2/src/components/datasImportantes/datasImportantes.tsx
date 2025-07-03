dda8b4011de65423f2bee48cd8f84b17648fe3ed
EduuIX <eduardomoraisribeiro@hotmail.com>
2025-07-03 13:50:07 -0300

    fix: atualizar datas importantes de submissão e aceitação
    Alteradas as datas de submissão de trabalhos, notificação de aceitação e envio da versão final para as novas datas:
    - Data limite de submissão de trabalhos: 16/07/2025
    - Data de notificação de aceitação: 30/07/2025
    - Data de envio da versão final: 07/08/2025

--- site-ERIN2/src/components/datasImportantes/datasImportantes.tsx
+++ site-ERIN2/src/components/datasImportantes/datasImportantes.tsx
@@ -7,9 +7,9 @@ const DatasImportantes = () => {
                 <h2>Datas Importantes </h2>
                 <ul>
                     <li><strong>Data de abertura das chamadas:</strong> 02/06/2025</li>
-                    <li><strong>Data limite de submissão de trabalhos:</strong> 05/07/2025</li>
-                    <li><strong>Data de notificação de aceitação:</strong> 20/07/2025</li>
-                    <li><strong>Data de envio da versão final:</strong> 01/08/2025</li>
+                    <li><strong>Data limite de submissão de trabalhos:</strong> 16/07/2025</li>
+                    <li><strong>Data de notificação de aceitação:</strong> 30/07/2025</li>
+                    <li><strong>Data de envio da versão final:</strong> 07/08/2025</li>
                 </ul>
             </div>
         </section>
